c7c827c0be517905ae688e7e847ec33a679ae30f
jona8690 <jona8690@edu.eal.dk>
2017-02-09 08:36:49 +0100

    a

---
+++ PractiseMakesPerfect01/Tools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseMakesPerfect01 {
+	class Tools {
+
+		public int GetBiggerInt(int a, int b) {
+			if (a > b) {
+				return a;
+			} else return b;
+		}
+
+
+
+	}
+}
